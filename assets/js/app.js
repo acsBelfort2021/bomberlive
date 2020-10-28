8e8d3d7d906cb9bec4bb03cb05ad10412b58b2f1
stagiaire_acs <victor.j.f.lapostolle@gmail.com>
2020-10-28 10:09:46 +0100

    amélioration de styles + création d'ennemis + détection d'une explosion pour tous les éléments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,86 @@
+//le joueur
 const player = document.getElementById("player");
+//le plateau de jeu
 const gameBoard = document.getElementById("gameboard");
+//est-ce que le joueur est touché ou non, par déafut à false
 let isTouched = false;
+//nombre de vies de notre personnage
 let lives = 3;
+//nombre d'ennemis
+const nbEnnemies = 10;
+//tableau qui va contenir nos ennemis (des divs)
 const ennemies = [];
 
-function generateRandomEnnemies() {
-    while(ennemies.length < 6) {
-        let x = Math.floor(Math.random() * (650 - 25 + 1)) + 25;
-        let y = Math.floor(Math.random() * (650 - 25 + 1)) + 25;
-        console.log(`x = ${x} et y = ${y}`);
-        ennemies.push([x, y]);
+//fonction pour générer des positions initiales aléatoires à nos ennemis
+function generateRandomPositions() {
+  //tableau qui va contenir nos différentes positions aléatoires initiales
+  const randomPositions = [];
+  //tant que ce tableau ne fait pas la taille du nombre d'ennemis définis
+  while (randomPositions.length < nbEnnemies) {
+    //on  créé des valeurs aléatoires pour une position sur l'axe x et y qui ont des valeurs multiples de 50, soit la longueur et largeur de nos éléments, et qui sont adaptés à la taille de notre plateau de jeu
+    x = Math.floor(Math.random() * 15) * 50 + 25; //+25 à cause du border du plateau de jeu
+    y = Math.floor(Math.random() * 15) * 50 + 25; //+25 à cause du border du plateau de jeu
+    //si ces valeurs ne sont pas trop proches du centre, soit la zone où se trouve notre personnage
+    if (!(x >= 250 && x <= 450) && !(y >= 250 && y <= 450)) {
+      //un booléen représentant si l'on peut ajouter le duo x, y dans notre tableau de positions
+      let add = true;
+      //on parcours notre tableau de positions aléatoire
+      for (let j = 0; j < randomPositions.length; j++) {
+        //si cette position existe déjà dans notre tableau de positions, càd qu'un ennemi a déjà cette position
+        if (randomPositions[j][0] === x && randomPositions[j][1] === y) {
+          //on empêche le fait de rajouter cette position dans notre tableau de position
+          add = false;
+        }
+      }
+      //si on a respecté toutes les conditions ==> add = true
+      if (add) {
+        //on ajoute le duo x, y dans notre tableau, ce qui incrémente pour notre boucle while
+        randomPositions.push([x, y]);
+      }
     }
-    console.log(ennemies);
+  }
+  //on retourne le tableau de positions aléatoires
+  return randomPositions;
 }
 
-generateRandomEnnemies();
+//fonction pour créer les ennemis
+function createEnnemies() {
+  //on récupère les positions aléatoires grâce à la fonction generateRandomPositions()
+  const positions = generateRandomPositions();
+  //pour le nombre d'ennemis que l'on souhaite
+  for (let i = 0; i < nbEnnemies; i++) {
+    //on créé une div
+    ennemies[i] = document.createElement("div");
+    //on lui ajoute la classe ennemi
+    ennemies[i].setAttribute("class", "ennemi");
+    //on lui donne un style inline pour son top
+    ennemies[i].style.top = positions[i][0] + "px";
+    //on lui donne un style inline pour son left
+    ennemies[i].style.left = positions[i][1] + "px";
+    //on ajoute l'ennemi au plateau de jeu
+    gameBoard.appendChild(ennemies[i]);
+  }
+}
 
+//on appelle la fonction pour créer les ennemis au chargement de la page
+createEnnemies();
+
+//fonction pour récupérer plus rapidement les valeurs calculées des propriétés CSS des éléments
 function getStyleValue(element, property) {
   return parseInt(window.getComputedStyle(element).getPropertyValue(property));
 }
 
+//fonction pour détecter si une explosion touche un élément de notre plateau de jeu
 function detecteExplosion(explosion) {
+  //on rédupère le top de notre explosion
   let explosionTop = getStyleValue(explosion, "top");
+  //on rédupère le left de notre explosion
   let explosionLeft = getStyleValue(explosion, "left");
+  //on rédupère le top de notre joueur
   let playerTop = getStyleValue(player, "top");
+  //on rédupère le left de notre joueur
   let playerLeft = getStyleValue(player, "left");
+  //si notre joueur est dans l'explosion et n'est pas dans des frames d'invulnérabilité (soit isTouched à true)
   if (
     playerTop >= explosionTop - 50 &&
     playerTop <= explosionTop + 50 &&
@@ -32,16 +88,35 @@ function detecteExplosion(explosion) {
     playerLeft <= explosionLeft + 50 &&
     !isTouched
   ) {
+    //on diminue son nombre de vie
     lives--;
+    //on passe le fait que le joueur soit touché à vrai
     isTouched = true;
     player.classList.add("touched");
-    setTimeout(function() {
-        isTouched = false;
-        player.classList.remove("touched");
+    setTimeout(function () {
+      isTouched = false;
+      player.classList.remove("touched");
     }, 3000);
+  }
+  //On parcours le tableau d'ennemis dans le sens inverse, cela est recommandé lorsque l'on désire retirer un élément d'un tableau au fur et à mesure qu'on le parcours
+  for (let i = ennemies.length - 1; i >= 0; i--) {
+    let ennemiTop = getStyleValue(ennemies[i], "top");
+    let ennemiLeft = getStyleValue(ennemies[i], "left");
+    console.log(`ennemi top = ${ennemiTop} left = ${ennemiLeft}`);
+    console.log(`bombe top = ${explosionTop} left = ${explosionLeft}`);
+    if (
+      ennemiTop >= explosionTop - 50 &&
+      ennemiTop <= explosionTop + 50 &&
+      ennemiLeft >= explosionLeft - 50 &&
+      ennemiLeft <= explosionLeft + 50
+    ) {
+      gameBoard.removeChild(ennemies[i]);
+      ennemies.splice(i, 1);
+    }
   }
 }
 
+//fonction pour créer une explosion
 function createExplosion(bombTop, bombLeft) {
   let explosion = document.createElement("div");
   explosion.setAttribute("class", "explosion");
@@ -54,6 +129,7 @@ function createExplosion(bombTop, bombLeft) {
   }, 300);
 }
 
+//fonction pour créer une bombe
 function createBomb(playerTop, playerLeft) {
   let bomb = document.createElement("div");
   bomb.setAttribute("class", "bomb");
@@ -66,6 +142,7 @@ function createBomb(playerTop, playerLeft) {
   }, 3000);
 }
 
+//On écoute si l'utilisateur appuie sur des touches de son clavier
 document.addEventListener("keydown", (e) => {
   let left = getStyleValue(player, "left");
   let top = getStyleValue(player, "top");
